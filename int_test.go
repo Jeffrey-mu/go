c364176db853ae537c4408a46529a3eabe269d12
mr-wang1009 <1115378579@qq.com>
2022-07-17 20:58:34 +0800

    feat: 添加整数test

---
+++ int_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	sum := Add(1, 2)
+	expected := 4
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
